2749d7c3ad3b43162214e1bab4574c2b0a94ac59
Facu Conejero <fgarciaconejero@gmail.com>
2018-10-12 10:47:01 -0300

    Primer commit

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre de usuario.");
+            string name = Console.ReadLine();
+
+            User newUser = new User(name);
+            
+            newUser.SetName(name);
+            Console.WriteLine("Ingrese su apellido.");
+            newUser.SetLastName(Console.ReadLine());
+
+            Console.WriteLine(newUser.GetFullName());
+            Console.ReadKey();
+        }
+    }
+}
